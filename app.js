082c778909f22b54f1d3f44a81dff32e91d6be30
Maxi Ortega <maximilianortega2000@gmail.com>
2026-08-31 16:52:43 -0300

    Actualizacion de app.script horarios listos

--- app.js
+++ app.js
@@ -535,7 +535,7 @@ comboButtons.forEach(btn => {
   /* ==========================================
      5. CALENDARIO & HORARIOS
   ========================================== */
-  const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyb2igtxzToG8w5pfGatfePHGqkAtbyOY2gWgg_EKPO9h4_d5SAP36fJH8Mhp6gKDw9/exec      ';
+  const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbztKTqpbceSXyXq9tAHAz9mtIzKVshl_7ZalkN-hJVkH68L151Ycqnwzy2RkeWwqh39/exec';
   let selectedDate = null;
   let selectedTime = null;
 
